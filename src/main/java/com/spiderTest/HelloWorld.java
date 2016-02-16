da2a5738d4dc2b53a9f0791d758d15fe07d62de1
loveluckystar <lixing89@sina.com>
2016-02-16 14:30:56 +0800

    修改helloworld1111

---
+++ src/main/java/com/spiderTest/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.spiderTest;
+
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Å¶À²À²");
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
